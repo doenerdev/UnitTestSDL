d343261fb206767bc87225f87e3feaeb2b67afba
doenerdev <spaghetticodemonster@proton.me>
2025-04-22 21:02:27 +0000

    restrucutre, added domain leakage example

---
+++ Tests/ResistanceToRefactoring/DomainLeakage/Aux.cs
@@ -0,0 +1,11 @@
+namespace Tests.ResistanceToRefactoring.DomainLeakage;
+
+public interface IConditionListRepository
+{
+    ConditionList GetConditionListById(Guid conditionListId);
+}
+
+public class ConditionList(decimal factor)
+{
+    public decimal Factor { get; } = factor;
+}
---
+++ Tests/ResistanceToRefactoring/DomainLeakage/ProdAndTest.cs
@@ -0,0 +1,57 @@
+﻿using Xunit;
+
+namespace Tests.ResistanceToRefactoring.DomainLeakage;
+
+//Productive Code
+public class Calculator(IConditionListRepository repository) 
+{
+    public decimal CalculateLeaseRate(
+        decimal baseInstallment, 
+        Guid conditionListId)
+    {
+        var conditionList = repository
+            .GetConditionListById(conditionListId);
+
+
+        return baseInstallment * conditionList.Factor;
+    }
+}
+
+
+//Test Code
+public class CalculatorTests
+{
+    [Fact]
+        public void CalculateLeaseRate_RateIsCalculatedWithFactor()
+    {
+        //Arrange
+        var factor = 1.5m;
+        var installment = 100m;
+        
+        var expected = factor * installment; //implementation or algorithm details leaked to the test
+
+        var stubRepo = new StubRepository(factor);
+        var sut = new Calculator(stubRepo);
+
+        //Act
+        var actual = sut.CalculateLeaseRate(installment, Guid.NewGuid());
+
+        //Act & Assert
+        Assert.Equal(expected, actual);
+	
+    }
+
+    private class StubRepository(decimal factor) 
+        : IConditionListRepository
+    {
+        public ConditionList GetConditionListById(
+            Guid conditionListId)
+        {
+            return new ConditionList(factor);
+        }
+    }
+
+}
+
+
+
