f5322ed3a40f252e3c7e2508933bed48e798a128
Nico Häffner <haeffnernico@gmail.com>
2025-04-23 15:40:43 +0200

    added fixture example

---
+++ Tests/Maintainability/Fixtures/ProdAndTest.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace Tests.Maintainability.Fixtures;
+
+
+//Test Code
+public class DatabaseFixture : IDisposable
+{
+    public DatabaseFixture()
+    {
+        //Setup
+    }
+
+    public void Dispose()
+    {
+        //Teardown
+    }
+}
+
+public class CalculatorTests : IClassFixture<DatabaseFixture>
+{
+    private readonly DatabaseFixture _databaseFixture;
+
+    public CalculatorTests(DatabaseFixture databaseFixture)
+    {
+        _databaseFixture = databaseFixture;
+    }
+
+    [Fact]
+    public void CalculateLeaseRate_RateIsCalculatedWithFactor()
+    {
+        //test something here
+        Assert.True(true);
+    }
+}
+
+
+
